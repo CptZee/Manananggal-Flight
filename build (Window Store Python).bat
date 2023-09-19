23d9861ac042863fe4bf2ca28c4d0c9c9151be7d
CptZee <68337605+CptZee@users.noreply.github.com>
2023-09-19 16:58:14 +0800

    Refactored

---
+++ build (Window Store Python).bat
@@ -0,0 +1,2 @@
+%USERPROFILE%\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\Scripts\pyinstaller "Main.py" --add-data "Assets;Assets" 
+@PAUSE
